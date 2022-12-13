db14e02dbc435eb7d7a322817745611000a030d7
Jhonata Nogueira <jhoownogueira@outlook.com>
2022-12-13 16:40:04 -0300

    :tada: Estrutura Inicial

---
+++ src/app.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
+import { CreateNotificationBody } from './create-notifications-body';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  getHello() {
+    return this.prisma.notifications.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: CreateNotificationBody) {
+    const { recipientId, content, category } = body;
+    await this.prisma.notifications.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      }
+    });
+  }
+}
